9bd78b709ecac5c30e01a069ac88ce1bd34b95a2
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2018-03-13 17:13:05 -0400

    creating_reading_writing changes

--- creating_reading_writing.py
+++ creating_reading_writing.py
@@ -7,7 +7,7 @@ def check_q1(ans):
 
 
 def answer_q1():
-    return """pd.DataFrame({'Apples': [30], 'Bananas': [21]})"""
+    print("""pd.DataFrame({'Apples': [30], 'Bananas': [21]})""")
 
 
 def check_q2(ans):
@@ -19,10 +19,10 @@ def check_q2(ans):
 
 
 def answer_q2():
-    return """pd.DataFrame(
+    print("""pd.DataFrame(
     {'Apples': [35, 41], 'Bananas': [21, 34]},
     index=['2017 Sales', '2018 Sales']
-)"""
+)""")
 
 
 def check_q3(ans):
@@ -33,9 +33,9 @@ def check_q3(ans):
 
 
 def answer_q3():
-    return """pd.Series(['4 cups', '1 cup', '2 large', '1 can'], 
+    print("""pd.Series(['4 cups', '1 cup', '2 large', '1 can'], 
 index=['Flour', 'Milk', 'Eggs', 'Spam'], 
-name='Dinner')"""
+name='Dinner')""")
 
 
 def check_q4(ans):
@@ -44,7 +44,7 @@ def check_q4(ans):
 
 
 def answer_q4():
-    return """pd.read_csv("../input/wine-reviews/winemag-data_first150k.csv", index_col=0)"""
+    print("""pd.read_csv("../input/wine-reviews/winemag-data_first150k.csv", index_col=0)""")
 
 
 def check_q5(ans):
@@ -54,28 +54,28 @@ def check_q5(ans):
 
 
 def answer_q5():
-    return """pd.read_excel("../input/publicassistance/xls_files_all/WICAgencies2014ytd.xls", 
-sheetname='Pregnant Women Participating')"""
+    print("""pd.read_excel("../input/publicassistance/xls_files_all/WICAgencies2014ytd.xls", 
+sheetname='Pregnant Women Participating')""")
 
 
-def check_q6(ans):
+def check_q6(ans=""):
     import os
     expected = pd.DataFrame({'Cows': [12, 20], 'Goats': [22, 19]}, index=['Year 1', 'Year 2'])
     return os.path.exists("cows_and_goats.csv") and pd.read_csv("cows_and_goats.csv", index_col=0).equals(expected)
 
 
 def answer_q6():
-    return """q6_df.to_csv("cows_and_goats.csv")"""
+    print("""q6_df.to_csv("cows_and_goats.csv")""")
 
 
 def check_q7(ans):
     import sqlite3
     conn = sqlite3.connect("../input/pitchfork-data/database.sqlite")
     expected = pd.read_sql_query("SELECT * FROM artists", conn)
-    ans.head().equals(expected.head())
+    return ans.head().equals(expected.head())
 
 
 def answer_q7():
-    return """import sqlite3
-sqlite3.connect("../input/pitchfork-data/database.sqlite")
-pd.read_sql_query("SELECT * FROM artists", conn)"""
+    print("""import sqlite3
+conn = sqlite3.connect("../input/pitchfork-data/database.sqlite")
+pd.read_sql_query("SELECT * FROM artists", conn)""")
